f9571f6ce192f08fbb655388c8ca5c3d4ef6a5d9
Ksenia Kashirina <kka.97@mail.ru>
2022-05-19 13:11:08 +0400

    second commit

--- homepage/views.py
+++ homepage/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods, require_GET
 
-from django.http import HttpResponse
 
-
+@require_http_methods(['GET', 'POST'])
 def index_page(request):
     return render(request, 'homepage/index.html')
 
 
+@require_GET
 def articles(request):
-    return HttpResponse('<h1>Articles</h1>')
+    args = {
+        'articles': list(range(1, 8)),
+        'val1': '<h3>Value1</h3>',
+        'val2': '<h4>Value2</h4>'
+    }
+    return render(request, 'homepage/articles.html', args)
